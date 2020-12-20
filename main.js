12337e3d702880100617822f956f60436cdb0a07
simpidbit <2766277616@qq.com>
2020-12-21 04:07:24 +0800

    v0.2.2

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-var music = document.getElementById("music-audio");
-music.volume = 0.05;
+window.onload = function() {
+    var music = document.getElementById("music-audio");
+    music.volume = 0.05;
+    
+    var musicinfo = document.getElementById("music-info");
+    
+    var randindex = Math.floor(Math.random() * 10 / 2);
+    var sounds = [
+            ["小星星变奏曲 | Wolfgang Amadeus Mozart", "./sounds/小星星变奏区.mp3"],
+            ["Bloom of Youth | 清水淳一", "./sounds/Bloom-of-Youth.mp3"],
+            ["Menuet D Major | Wolfgang Amadeus Mozart", "./sounds/Menuet-D-Major.mp3"],
+            ["Nuit Silencieuse | Days", "./sounds/Nuit-Silencieuse.mp3"],
+            ["The Right Path | Thomas Greenberg", "./sounds/The-Right-Path.mp3"]
+        ];
+    
 
-var musicinfo = document.getElementById("music-info");
-
-var randindex = Math.floor(Math.random() / 2);
-var sounds = [
-        ["小星星变奏曲 | Wolfgang Amadeus Mozart", "./sounds/小星星变奏区.mp3"],
-        ["Bloom of Youth | 清水淳一", "./sounds/Bloom-of-Youth.mp3"],
-        ["Menuet D Major | Wolfgang Amadeus Mozart", "./sounds/Menuet-D-Major.mp3"],
-        ["Nuit Silencieuse | Days", "./sounds/Nuit-Silencieuse.mp3"],
-        ["The Right Path | Thomas Greenberg", "./sounds/The-Right-Path.mp3"]
-    ];
-
-
-musicinfo.innerHTML = sounds[randindex][0];
-music.src = sounds[randindex][1];
+    musicinfo.innerHTML = sounds[randindex][0];
+    music.src = sounds[randindex][1];
+    location.reload();
+}
